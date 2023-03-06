bf018224991b230586d26fab046c623fda7f39e1
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-06 21:05:24 +0900

    Update TransactionEnum.java

--- bank/src/main/java/shop/mtcoding/bank/domain/TransactionEnum.java
+++ bank/src/main/java/shop/mtcoding/bank/domain/TransactionEnum.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum TransactionEnum {
 
-    WITHDRAW("출금"), DEPOSIT("입금"), TRANSFER("이체"), ALL("입출금내역"),
+    WITHDRAW("출금"), DEPOSIT("입금"), TRANSFER("이체"), ALL("입출금내역");
 
     private String value;
 }
