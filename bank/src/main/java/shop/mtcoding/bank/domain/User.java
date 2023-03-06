a53597d03d214ada8a19c12db1810774dbd33b7e
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-06 17:10:53 +0900

    Created User 엔티티

---
+++ bank/src/main/java/shop/mtcoding/bank/domain/User.java
@@ -0,0 +1,60 @@
+package shop.mtcoding.bank.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_tb")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(unique = false, length = 60) // 패스워드 인코딩(BCrypt)
+    private String password;
+
+    @Column(unique = false, length = 20)
+    private String email;
+
+    @Column(unique = false, length = 20)
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserEnum role; // ADMIN, CUSTOMER
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Long id, String username, String password, String email, String fullname, UserEnum role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
