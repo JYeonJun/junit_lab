504da3606a76831800345868df346afd634a6bcd
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-06 21:35:05 +0900

    Create CustomResponseUtil.java

---
+++ bank/src/main/java/shop/mtcoding/bank/util/CustomResponseUtil.java
@@ -0,0 +1,39 @@
+package shop.mtcoding.bank.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import shop.mtcoding.bank.dto.ResponseDto;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomResponseUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomResponseUtil.class);
+
+    public static void unAuthentication(HttpServletResponse response, String msg) {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            ResponseDto<String> responseDto = new ResponseDto<>(-1, msg, null);
+            String responseBody = om.writeValueAsString(responseDto);
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(401);
+            response.getWriter().println(responseBody);
+        } catch (Exception e) {
+            log.error("서버 파싱 에러");
+        }
+    }
+
+    /*public static void unAuthorization(HttpServletResponse response, String msg) {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            ResponseDto<String> responseDto = new ResponseDto<>(-1, msg, null);
+            String responseBody = om.writeValueAsString(responseDto);
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(403);
+            response.getWriter().println(responseBody);
+        } catch (Exception e) {
+            log.error("서버 파싱 에러");
+        }
+    }*/
+}
