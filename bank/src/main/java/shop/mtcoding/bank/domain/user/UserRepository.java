9a04df69569860b264db605d2b66b6af1da60df1
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-07 10:57:25 +0900

    Created Repository

---
+++ bank/src/main/java/shop/mtcoding/bank/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package shop.mtcoding.bank.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByUsername(String username);
+}
