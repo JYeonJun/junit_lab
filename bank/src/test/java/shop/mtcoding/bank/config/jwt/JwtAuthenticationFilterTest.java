6699eb740601466f08b28f4263a1c2540ceaf141
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-11 18:59:22 +0900

    authentication 테스트

--- bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -1,4 +1,100 @@
 package shop.mtcoding.bank.config.jwt;
 
-public class JwtAuthenticationFilterTest {
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static shop.mtcoding.bank.dto.user.UserReqDto.*;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@Transactional
+class JwtAuthenticationFilterTest extends DummyObject{
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        User ssar = userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    @Test
+    public void successfulAuthentication_test() throws Exception {
+
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("1234");
+
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/login")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("responseBody = " + responseBody);
+        System.out.println("jwtToken = " + jwtToken);
+
+        // then
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+        assertThat(jwtToken).isNotNull();
+        assertThat(jwtToken.startsWith(JwtVO.TOKEN_PREFIX)).isTrue();
+    }
+
+    @Test
+    public void unSuccessfulAuthentication_test() throws Exception {
+
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("");
+        loginReqDto.setPassword("12345");
+
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/login")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("responseBody = " + responseBody);
+        System.out.println("jwtToken = " + jwtToken);
+
+
+        // then
+        resultActions.andExpect(status().isUnauthorized());
+    }
 }
